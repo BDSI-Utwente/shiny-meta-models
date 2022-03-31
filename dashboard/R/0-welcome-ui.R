a61712e1e2e3bb0c014626a54ad4aabee1de4bae
Pouwels <x.g.l.v.pouwels@utwente.nl>
2022-03-31 07:46:23 +0200

    Start welcome text

--- dashboard/R/0-welcome-ui.R
+++ dashboard/R/0-welcome-ui.R
@@ -4,10 +4,17 @@ library(shiny)
 welcomeUI <- tabItem(
   "welcome",
   box(
-    title = "Welcome to the PASHBOARD",
+    title = "Welcome to the PACBOARD",
     status = "primary",
     width = 12,
-    p("There will be some explanation here"),
-    p("lorem ipsum")
+    p("This Probabilistic Analysis Check dashBOARD (PACBOARD) is an interactive tool to systematically validate and explore the results of a probabilistic analysis from a health economic model."),
+    p("To use PACBOARD, you need a dataset (or dataframe) containing both inputs and (intermediate) outputs of a probabilistic analysis. The column of the dataset should represent the input or output name."),
+    p("PACBOARD is composed of 5 different tabs."),
+    p("The Prepare data-tab: upload your data and group variables in 'costs', 'utilities', and 'probabilities'. You can also define whether you want to calculate the net benefits if the dataset does not contain it already."),
+    p("The Summary statistics-tab: obtain summary statistics of the inputs and outputs, investigate the correlation between inputs and outputs, and perform the first quick check on your inputs and outputs."),
+    p("The Model outcomes-tab: displays the incremental cost-effectiveness plane and the cost-effectiveness acceptability curve and other visuals to investigate your model outcomes."),
+    p("The Relations-tab: investigate the relation between model inputs and outputs using interactive graphs and metamodelling."),
+    p("The Meta-model predictions-tab: use the metamodel you have fitted to make predictions."),
+    p("In case you are only curious, you can use the pre-loaded data, by clicking the 'Use test data' button on the 'Prepare data'-tab.")
   )
 )
