fd94949ab8e08aa3ca1d4783979895f77f30735f
Pouwels <x.g.l.v.pouwels@utwente.nl>
2022-06-29 17:17:03 +0200

    change description

--- dashboard/R/0-welcome-ui.R
+++ dashboard/R/0-welcome-ui.R
@@ -13,8 +13,8 @@ welcomeUI <- tabItem(
     p("The Prepare data-tab: upload your data and group variables in 'costs', '(dis)utilities', 'probabilities', and 'relative effectiveness'. In this tab, the first quick checks are performed on your inputs and outputs You can also define whether you want to calculate the net benefits if the dataset does not contain it already."),
     p("The Data inspection-tab: obtain summary statistics of the inputs and outputs, investigate the correlation between inputs and outputs."),
     p("The Model outcomes-tab: displays the incremental cost-effectiveness plane and the cost-effectiveness acceptability curve and other visuals to investigate your model outcomes."),
-    p("The Relations-tab: investigate the relation between model inputs and outputs using interactive graphs and metamodelling."),
-    p("The Meta-model predictions-tab: use the metamodel you have fitted to make predictions."),
+    p("The Metamodelling-tab: investigate the relation between model inputs and outputs using interactive graphs and metamodelling."),
+    p("The Metamodel predictions-tab: use the metamodel you have fitted to make predictions."),
     p("In case you are only curious, you can use the pre-loaded data, by clicking the 'Use test data' button on the top of the tabs.")
   )
 )
