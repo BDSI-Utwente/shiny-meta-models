1cb4402c636c2743a2156661664416200c8a7901
Pouwels <x.g.l.v.pouwels@utwente.nl>
2022-07-04 09:13:35 +0200

    start survival analysis tab

---
+++ dashboard/R/6-survival.R
@@ -0,0 +1,13 @@
+# UI ----
+survivalUI <- tabItem(
+  "survival",
+  box(
+    width = 12,
+    title = "Survival analysis check"
+  )
+)
+
+# SERVER ----
+survivalServer <- function(input, output, session, context) {
+  
+  }
