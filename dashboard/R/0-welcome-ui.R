c2dcc15d9c8afb39728b033d1b7b907d217331c3
Pouwels <x.g.l.v.pouwels@utwente.nl>
2022-07-05 08:21:14 +0200

    corrected typo

--- dashboard/R/0-welcome-ui.R
+++ dashboard/R/0-welcome-ui.R
@@ -8,7 +8,7 @@ welcomeUI <- tabItem(
     status = "primary",
     width = 12,
     p("The Probabilistic Analysis Check dashBOARD (PACBOARD) is an interactive tool to systematically validate and explore the results of a probabilistic analysis from a health economic model."),
-    p("To use PACBOARD, you need a dataset (or dataframe) containing both inputs and (intermediate) outputs of a probabilistic analysis. The column of the dataset should represent the input or output name."),
+    p("To use PACBOARD, you need a dataset (or dataframe) containing both inputs and (intermediate) outputs of a probabilistic analysis. The first row of the dataset should represent the input or output name."),
     p("PACBOARD is composed of 5 different tabs."),
     p("The Prepare data-tab: upload your data and group variables in 'costs', '(dis)utilities', 'probabilities', and 'relative effectiveness'. In this tab, the first quick checks are performed on your inputs and outputs You can also define whether you want to calculate the net benefits if the dataset does not contain it already."),
     p("The Data inspection-tab: obtain summary statistics of the inputs and outputs, investigate the correlation between inputs and outputs."),
